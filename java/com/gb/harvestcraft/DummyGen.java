ca2b9ba08704fdcb5b041358f1f4780cc0048c8d
gbromios <gbromios@example.com>
2015-10-02 21:17:52 -0400

    made a weighted random type because mojangs was terribad

--- java/com/gb/harvestcraft/DummyGen.java
+++ java/com/gb/harvestcraft/DummyGen.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 
 import com.gb.harvestcraft.garden.Gardens;
 import com.gb.harvestcraft.garden.block.BlockGarden;
 import com.gb.harvestcraft.veggie.Veggies;
+import com.gb.util.WeightedArray;
+import static com.gb.util.WeightedArray.Choice;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSand;
@@ -18,42 +21,29 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import net.minecraft.util.WeightedRandom;
 
 public class DummyGen implements IWorldGenerator {
 	// plains, swamp, desert, river and mushroom island are handled individually no need to put em in the set
 	private final HashSet<BiomeGenBase> never_biomes;
-	
+
 	private final HashSet<BiomeGenBase> taiga_biomes;
-	private final ArrayList<DummyGen.GardenWTFWR> taiga_gardens;
 	private final HashSet<BiomeGenBase> forest_biomes;
-	private final ArrayList<DummyGen.GardenWTFWR> forest_gardens;
 	private final HashSet<BiomeGenBase> mountain_biomes;
-	private final ArrayList<DummyGen.GardenWTFWR> mountain_gardens;
 	private final HashSet<BiomeGenBase> arid_biomes;
-	private final ArrayList<DummyGen.GardenWTFWR> arid_gardens;
 	private final HashSet<BiomeGenBase> lush_biomes;
-	private final ArrayList<DummyGen.GardenWTFWR> lush_gardens;
-	
-	private final ArrayList<DummyGen.GardenWTFWR> plains_gardens;
-	private final ArrayList<DummyGen.GardenWTFWR> swamp_gardens;
-	
-	static final class GardenWTFWR extends WeightedRandom.Item {
-		public final BlockGarden garden;
-		public GardenWTFWR(BlockGarden garden, int weight) {
-			super(weight);
-			this.garden = garden;
-		}
-	}
-	
-	static final class TUMMYGRABBER extends WeightedRandom {
-		public BlockGarden getRandomGarden(Random r, Collection c) {
-			return ((GardenWTFWR)super.getRandomItem(r, c)).garden;
-		}
-	}
+	// swamp and plains only have 1 biome each, so they don't need a mapping
+
+	private final WeightedArray<BlockGarden> taiga_gardens;
+	private final WeightedArray<BlockGarden> forest_gardens;
+	private final WeightedArray<BlockGarden> mountain_gardens;
+	private final WeightedArray<BlockGarden> arid_gardens;
+	private final WeightedArray<BlockGarden> lush_gardens;
+	private final WeightedArray<BlockGarden> plains_gardens;
+	private final WeightedArray<BlockGarden> swamp_gardens;
 
 
 	public DummyGen(){
+		//region // map biomes to categories
 		// no fruits or veggies should spawn here
 		never_biomes = new HashSet<BiomeGenBase>(Arrays.asList(new BiomeGenBase[]{
 				BiomeGenBase.beach,
@@ -104,55 +94,59 @@ public class DummyGen implements IWorldGenerator {
 				BiomeGenBase.jungleHills,
 				BiomeGenBase.jungleEdge
 		}));
-		
-		//
-        taiga_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.allium, 30),
-        		new DummyGen.GardenWTFWR(Gardens.ground, 10),
-        		new DummyGen.GardenWTFWR(Gardens.gourd, 10)
-        }));
-        forest_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.berry, 20),
-        		new DummyGen.GardenWTFWR(Gardens.herb, 10),
-        		new DummyGen.GardenWTFWR(Gardens.stalk, 10),
-        		new DummyGen.GardenWTFWR(Gardens.allium, 10)
-        		
-        }));
-        mountain_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.allium, 20),
-        		new DummyGen.GardenWTFWR(Gardens.berry, 20),
-        		new DummyGen.GardenWTFWR(Gardens.gourd, 5),
-        		new DummyGen.GardenWTFWR(Gardens.grass, 5)
-        }));
-        arid_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.grass, 40),
-        		new DummyGen.GardenWTFWR(Gardens.ground, 40),
-        		new DummyGen.GardenWTFWR(Gardens.stalk, 20)
 
-        }));
-        lush_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.berry, 20),
-        		new DummyGen.GardenWTFWR(Gardens.tropical, 40),
-        		new DummyGen.GardenWTFWR(Gardens.leafy, 20),
-        		new DummyGen.GardenWTFWR(Gardens.stalk, 20),
-        		new DummyGen.GardenWTFWR(Gardens.herb, 20)
-        		
-        }));
-        plains_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.grass, 40),
-        		new DummyGen.GardenWTFWR(Gardens.leafy, 15),
-        		new DummyGen.GardenWTFWR(Gardens.textile, 10),
-        		new DummyGen.GardenWTFWR(Gardens.gourd, 10)
-        }));
-        swamp_gardens = new ArrayList<DummyGen.GardenWTFWR>(Arrays.asList(new DummyGen.GardenWTFWR[]{
-        		new DummyGen.GardenWTFWR(Gardens.leafy, 30),
-        		new DummyGen.GardenWTFWR(Gardens.herb, 30),
-        		new DummyGen.GardenWTFWR(Gardens.stalk, 30),
-        		new DummyGen.GardenWTFWR(Gardens.tropical, 10)
-        }));
-        
-        
+		//endregion
 
+
+		// region // map gardens to biomes
+		taiga_gardens = new WeightedArray<BlockGarden>()
+				.add(Gardens.allium, 30)
+				.add(Gardens.ground, 20)
+				.add(Gardens.herb, 10)
+				;
+
+		forest_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.berry, 25)
+			.add(Gardens.stalk, 10)
+			.add(Gardens.allium, 10)
+			.add(Gardens.ground, 10)
+
+		;
+		mountain_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.allium, 20)
+			.add(Gardens.berry, 20)
+			.add(Gardens.gourd, 5)
+			.add(Gardens.grass, 5)
+		;
+		arid_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.grass, 40)
+			.add(Gardens.ground, 40)
+			.add(Gardens.stalk, 20)
+
+		;
+		lush_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.berry, 20)
+			.add(Gardens.tropical, 40)
+			.add(Gardens.leafy, 20)
+			.add(Gardens.stalk, 20)
+			.add(Gardens.herb, 20)
+
+		;
+		plains_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.grass, 40)
+			.add(Gardens.leafy, 15)
+			.add(Gardens.textile, 10)
+			.add(Gardens.gourd, 10)
+		;
+		swamp_gardens = new WeightedArray<BlockGarden>()
+			.add(Gardens.leafy, 30)
+			.add(Gardens.herb, 30)
+			.add(Gardens.stalk, 30)
+			.add(Gardens.tropical, 10)
+		;
+
+
+		//endregion
 
 	}
 
@@ -165,14 +159,16 @@ public class DummyGen implements IWorldGenerator {
 		// I'll implement it nicely when BoP rises from the ashes~!
 
 		// 1/16 chance to gen a garden
-		if (random.nextInt(15) > 0) {
+		
+		// its uncoditional right now to test generation logic. should try once per chunk
+		//if (random.nextInt(15) > 0) {
 			this.genGarden(
 					random,
 					world,
 					(chunkX * 16) + random.nextInt(15),
 					(chunkZ * 16) + random.nextInt(15), 
 					world.getBiomeGenForCoords(new BlockPos((chunkX * 16) + 7, 0, (chunkZ * 16) + 7)));
-		}
+		//}
 
 		// same for fruit but since leaves are rarer, so shall be the fruits
 		if (random.nextInt(15) > 0) {
@@ -184,15 +180,22 @@ public class DummyGen implements IWorldGenerator {
 					world.getBiomeGenForCoords(new BlockPos((chunkX * 16) + 7, 0, (chunkZ * 16) + 7)));
 		}				
 	}
-	
+
 	private BlockPos findLowestAir(World world, int x, int z){
-		BlockPos bp;
+		BlockPos pos;
 		int y = 128;
 		while (y-- > 42){
-			bp = new BlockPos(x, y, z);
-			if (!world.isAirBlock(bp.down())) {
-				return bp;
+			pos = new BlockPos(x, y, z);
+			if (!world.isAirBlock(pos.down())) {
+				// mojang pls
+				if (world.getBlockState(pos.down()).getBlock().isLeaves(world, pos.down())) {
+					System.out.format(" <><> FOLIAGE @@@ %s\n", pos.down());
+					continue;
+				}
+				return pos;
 			}
+
+
 		}
 		return null;
 	}
@@ -203,36 +206,26 @@ public class DummyGen implements IWorldGenerator {
 			System.out.format(" ? no air at %d, %d???\n", x, z);
 			return;
 		}
-		
-		
+
+
 		if (pos.getY() < 40){
 			System.out.format("   cannot :( - '%d' < 40~ 2 deep 4 me\n", pos.getY());
 			return; 
 		}
-		
+
 		Block soil = world.getBlockState(pos.down()).getBlock();
 		//System.out.format(" ~ trying genGarden in %s @ %s:\n", biome.biomeName, pos);
-		
-		// ugh where do i put this crap
-		TUMMYGRABBER wr = new TUMMYGRABBER();
-
-
-		// make sure there's room to grow
-		//if (!world.isAirBlock(pos.up())){
-		//	System.out.format("   cannot :( - '%s' in the way\n", world.getBlockState(pos));	
-		//	return;
-		//}
 
 		// some biomes just don't get veggies >:C
 		if (never_biomes.contains(biome)) {
 			//System.out.format("   NEVER IN %s!\n", biome.biomeName);
 			return; 
-			}
+		}
 
 		// deserts have an even lower chance to grow 
 		if (biome.equals(BiomeGenBase.desert)) {
 			if (random.nextInt(3) == 0 && soil == Blocks.sand){
-				System.out.format(" ! GO DESERT !\n", biome.biomeName, pos);
+				System.out.format(" ! GO DESERT  @ %s %s!\n", biome.biomeName, pos);
 				world.setBlockState(pos, Gardens.desert.getDefaultState());
 			}
 			return; 
@@ -241,66 +234,66 @@ public class DummyGen implements IWorldGenerator {
 		// river crops are even more rarer
 		if (biome.equals(BiomeGenBase.river)) {
 			if (random.nextInt(7) == 0){
-				System.out.format(" ! GO RIVER !\n", biome.biomeName, pos);
+				System.out.format(" ! GO RIVER  @ %s %s!\n", biome.biomeName, pos);
 				world.setBlockState(pos, Gardens.water.getDefaultState());
 			}
 			return;
 		}
-		
+
 		if (mountain_biomes.contains(biome)) {
 			if (random.nextInt(7) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO MOUNTAINE !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, mountain_gardens).getDefaultState());
+				System.out.format(" ! GO MOUNTAINE @ %s %s !\n", biome.biomeName, pos);
+				world.setBlockState(pos, mountain_gardens.getRandom(random).getDefaultState());
 			}
 			return; 
 		}
-		
+
 		if (taiga_biomes.contains(biome)) {
 			if (random.nextInt(7) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO TIGERS! USA #1 !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, taiga_gardens).getDefaultState());
+				System.out.format(" ! GO TIGERS! @ %s %s USA #1 !\n", biome.biomeName, pos);
+				world.setBlockState(pos, taiga_gardens.getRandom(random).getDefaultState());
 			}
 			return; 
 		}
-		
+
 		if (arid_biomes.contains(biome)) {
 			if (random.nextInt(5) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO ARID !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, arid_gardens).getDefaultState());
+				System.out.format(" ! GO ARID  @ %s %s!\n", biome.biomeName, pos);
+				world.setBlockState(pos, arid_gardens.getRandom(random).getDefaultState());
 			}
 			return; 
 		}
-		
+
 		// at this point, the unchecked biomes are lush enough for a small chance to
 		// grow river gardens? :O
-		
+
 		if (forest_biomes.contains(biome)) {
 			if (random.nextInt(2) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO FORAST !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, forest_gardens).getDefaultState());
+				System.out.format(" ! GO FORAST @ %s %s !\n", biome.biomeName, pos);
+				world.setBlockState(pos, forest_gardens.getRandom(random).getDefaultState());
 			}
 			return; 
 		}
-		
+
 		if (lush_biomes.contains(biome)) {
 			if (random.nextInt(1) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO JUNGO !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, lush_gardens).getDefaultState());
+				System.out.format(" ! GO JUNGO @ %s %s !\n", biome.biomeName, pos);
+				world.setBlockState(pos, lush_gardens.getRandom(random).getDefaultState());
 			}
 			return; 
 		}
-		
+
 		if (BiomeGenBase.plains == biome){
 			if (random.nextInt(2) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO PLAINS !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, plains_gardens).getDefaultState());
+				System.out.format(" ! GO PLAINS @ %s %s !\n", biome.biomeName, pos);
+				world.setBlockState(pos, plains_gardens.getRandom(random).getDefaultState());
 			}
 			return;			
 		}
 		if (BiomeGenBase.swampland == biome){
 			if (random.nextInt(3) == 0 && (soil == Blocks.dirt || soil == Blocks.grass)){
-				System.out.format(" ! GO SKWUMP !\n", biome.biomeName, pos);
-				world.setBlockState(pos, wr.getRandomGarden(random, plains_gardens).getDefaultState());
+				System.out.format(" ! GO SKWUMP @ %s %s !\n", biome.biomeName, pos);
+				world.setBlockState(pos, plains_gardens.getRandom(random).getDefaultState());
 			}
 			return;			
 		}
@@ -316,7 +309,7 @@ public class DummyGen implements IWorldGenerator {
 		//if (pos.getY() < 40){ return; }
 
 	}
-	
-	
+
+
 
 }
---
+++ java/com/gb/util/WeightedArray.java
@@ -0,0 +1,41 @@
+package com.gb.util;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.gb.harvestcraft.garden.block.BlockGarden;
+
+public class WeightedArray<T> {
+	private ArrayList<Choice<T>> choices = new ArrayList<Choice<T>>(); 
+	private int totalWeight = 0;
+	public WeightedArray(){}
+
+	// returns this to allow lazy in line defining. why cant i generic array :(
+	public WeightedArray<T> add(T t, int weight){
+		this.choices.add(new Choice<T>(t, weight));
+		this.totalWeight += weight;
+		return this;
+	}
+
+	public T getRandom(Random r) {
+		int i = r.nextInt(this.totalWeight);
+		for (Choice<T> c : this.choices) {
+			if ((i -= c.weight) < 0)	{
+				return c.choice;
+			}
+		}
+		throw new RuntimeException("this should never happen, there's a bug in WeightedArray :(");
+	}
+	
+	public static final class Choice<T>{
+		public final T choice;
+		public final int weight;
+		public Choice(T choice, int weight) {
+			this.weight = weight;
+			this.choice = choice;
+		}
+	}
+
+	
+}
+
