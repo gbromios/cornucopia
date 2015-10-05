57e88792d9b321ff7a62ea218585ed4a1d3664fe
gbromios <gbromios@example.com>
2015-10-05 19:33:47 -0400

    expose mod instance and add a creative tab for cookery blocks

--- java/com/gb/harvestcraft/HarvestCraft.java
+++ java/com/gb/harvestcraft/HarvestCraft.java
@@ -11,6 +11,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -23,6 +24,9 @@ public class HarvestCraft
 {
     public static final String MODID = "harvestcraft";
     public static final String VERSION = "1.0";
+    
+    @Instance(value = HarvestCraft.MODID)
+    public static HarvestCraft instance;
    
     //region creative tabs
     
@@ -74,9 +78,15 @@ public class HarvestCraft
 			return Item.getItemFromBlock(Fruits.peach.leaf);
 		}
     };
+    public static final CreativeTabs tabCookeryBlock = new CreativeTabs("hc_cookeryblock"){
+		@Override
+		public Item getTabIconItem() {
+			return Item.getItemFromBlock(Fruits.peach.leaf); // TODO: obviously update this once the blocks are in
+		}
+    };
     
     //endregion
-    
+ 
     @SidedProxy(clientSide="com.gb.harvestcraft.ClientProxy", serverSide="com.gb.harvestcraft.ServerProxy")
     public static ServerProxy proxy;
 
