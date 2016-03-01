b1dba2b0f12e300e8fe4090e7809d49c3ee8348a
gbromios <gbromios@example.com>
2016-02-29 22:18:01 -0500

    increment version

--- src/main/java/com/gb/cornucopia/CornuCopia.java
+++ src/main/java/com/gb/cornucopia/CornuCopia.java
@@ -23,7 +23,7 @@ public class CornuCopia
 {
 	public static final String NAME = "CornuCopia";
 	public static final String MODID = "cornucopia";
-	public static final String VERSION = "1.1.13";
+	public static final String VERSION = "1.1.14";
 
 	@Instance(value = CornuCopia.MODID)
 	public static CornuCopia instance;
